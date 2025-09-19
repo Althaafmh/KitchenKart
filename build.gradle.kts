c6e1ce0f458ffd88f80326eb0785b007d2ae2824
Deshan Hettiarachchi <bingudeshan009@gmail.com>
2025-09-19 20:41:58 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    // These plugins are applied to sub-projects like 'app'
+    id("com.android.application") version "8.8.2" apply false
+    id("com.android.library") version "8.8.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
+}
